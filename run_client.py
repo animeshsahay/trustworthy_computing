e082a992ec2db8756f817f7d430553fbf45c7831
rockmanu <62845346+rockmanu@users.noreply.github.com>
2020-11-20 02:44:21 +0530

    Add files via upload

---
+++ run_client.py
@@ -0,0 +1,15 @@
+import socket
+import ssl
+import sys
+sys.path.insert(0, '/home/pi/trustworthy_computing/src')
+
+import config
+from client import Client
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+s = ssl.wrap_socket(s)                                        # wrap socket to secure connection
+
+s.connect(('192.168.1.25', 8080))                           # connect to sever
+
+client = Client(s)                                            # create new client object
+client.handle_connection()                                    # handle connection with server
