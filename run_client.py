2037d86c71a5fd7190a6131eb505b625e989c409
rockmanu <62845346+rockmanu@users.noreply.github.com>
2020-11-22 00:04:17 +0530

    Add files via upload

---
+++ run_client.py
@@ -0,0 +1,16 @@
+import socket
+import ssl
+import sys
+
+#sys.path.insert(0, '/home/pi/trustworthy_computing/src')
+
+#import config
+from client import Client
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+s = ssl.wrap_socket(s)  # wrap socket to secure connection
+
+s.connect(('192.168.1.29', 8081))  # connect to sever
+
+client = Client(s)  # create new client object
+client.handle_connection()  # handle connection with server
---
+++ run_server.py
@@ -0,0 +1,28 @@
+# run server with secure connection
+import socket
+from OpenSSL import SSL
+
+import sys
+
+#sys.path.insert(0, '/mnt/d/Project/trustworthy_computing-integration/src')
+#sys.path.insert(0, '/mnt/d/Project/trustworthy_computing-integration/src')
+#import src.database as database
+import database
+from server_update import ClientHandler
+
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('keys/key')
+context.use_certificate_file('keys/cert')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a socket object
+s = SSL.Connection(context, s)  # secure connection
+
+s.bind(('', 8080))  # bind to the port
+
+s.listen(5)  # wait for client connection.
+database.create_table()  # create table if not exist on application start
+
+while True:
+    client_socket, address = s.accept()  # Establish connection with client
+    clientThread = ClientHandler(client_socket)  # create a thread for each user
+    clientThread.start()  # run thread
